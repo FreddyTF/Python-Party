18eded70a4f807454e97548ff4a461c50994c887
JoDi-2903 <jonathan.diebel@web.de>
2022-04-26 19:32:18 +0200

    set up export

---
+++ ImportAndExport/export.py
@@ -0,0 +1,11 @@
+# ! encoding-UTF-8
+"""export.py
+Export data model to a JSON file
+
+author: inf20026@lehre.dhbw-stuttgart.de
+date: 26.04.22
+version: 0.0.1
+license: MIT
+"""
+
+from ..DataManager.Datamodel import Person, Spielfeld, Party, Beziehung
