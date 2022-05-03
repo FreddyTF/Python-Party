b6e45ab3bf53f890e05bdd465d5861f9b902ea09
JoDi-2903 <jonathan.diebel@web.de>
2022-05-03 09:46:15 +0200

    fixed reference to parent directory and tested export to json

--- ImportAndExport/export.py
+++ ImportAndExport/export.py
@@ -8,4 +8,18 @@ version: 0.0.1
 license: MIT
 """
 
-from ..DataManager.Datamodel import Person, Spielfeld, Party, Beziehung
+import os, sys
+import json
+
+# Make it possible to reference a module in the parent directory
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+# Import global datamodel
+from DataManager.Datamodel import Person, Spielfeld, Party, Beziehung
+
+Person1 = Person(1, 'Peter', 'Prinzessin', 'P', (1,1), (0,0), ['Test1', 'Test2'], 0)
+#convert to JSON string
+jsonStr = json.dumps(Person1.__dict__)
+print(jsonStr)
